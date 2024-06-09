bc3e91b34bf3f4684564143296ab141786646929
promanagement-id <aryapriambodo7@gmail.com>
2024-06-09 09:37:10 +0700

    feat: final revision..

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,6 +122,26 @@ function finalVideo2() {
 }
 videoFile2.addEventListener("ended", finalVideo2);
 
+const videoFile3 = document.getElementById("video-file-3"),
+  videoButton3 = document.getElementById("video-button-3"),
+  videoIcon3 = document.getElementById("video-icon-3");
+function playPause3() {
+  if (videoFile3.paused) {
+    videoFile3.play();
+    videoIcon3.classList.add("ri-pause-line");
+    videoIcon3.classList.remove("ri-play-line");
+  } else {
+    videoFile3.pause();
+    videoIcon3.classList.remove("ri-pause-line");
+    videoIcon3.classList.add("ri-play-line");
+  }
+}
+videoButton3.addEventListener("click", playPause3);
+function finalVideo3() {
+  videoIcon3.classList.remove("ri-pause-line");
+  videoIcon3.classList.add("ri-play-line");
+}
+videoFile3.addEventListener("ended", finalVideo3);
 
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
